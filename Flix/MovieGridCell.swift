9fa9a82211a2ca0143254cdfd0b92540aaa2a38a
Kharissa <kharissa@users.noreply.github.com>
2021-10-05 01:01:58 +0200

    update tab navigation and movie details

---
+++ Flix/MovieGridCell.swift
@@ -0,0 +1,13 @@
+//
+//  MovieGridCell.swift
+//  Flix
+//
+//  Created by Kharissa Fernando Smith on 10/4/21.
+//
+
+import UIKit
+
+class MovieGridCell: UICollectionViewCell {
+    
+    @IBOutlet weak var posterView: UIImageView!
+}
